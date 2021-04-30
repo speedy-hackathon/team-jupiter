36a8e56c49bba6ad8e05f29be07d7617dc1cfd3f
Andrew Vdovichenko <andrew.vdovichenko@gmail.com>
2021-04-30 22:51:50 +0500

    add doctor color to person

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -6,9 +6,11 @@ import classNames from "classnames";
 export default function Person({person, onClick}) {
     const x = (person.position.x / MAX_WIDTH) * 100;
     const y = (person.position.y / MAX_HEIGHT) * 100;
+    const isDoctor = person.profession === 'Doctor';
+
     return (
         <div
-            className={classNames(styles.root, {[styles.sickPerson]: person.isSick, [styles.boredPerson]: person.isBored})}
+            className={classNames(styles.root, {[styles.sickPerson]: person.isSick, [styles.boredPerson]: person.isBored, [styles.doctor]: isDoctor })}
             style={{left: `${x}%`, top: `${y}%`}}
             onClick={() => onClick(person.id)}
         />
