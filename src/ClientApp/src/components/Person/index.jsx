6dbf5beff79ff3e5cc61853e2f600486ac4f3ea5
Mayrinika <mashuliy_u@mail.ru>
2021-04-30 20:54:58 +0500

    add color for bored person

--- src/ClientApp/src/components/Person/index.jsx
+++ src/ClientApp/src/components/Person/index.jsx
@@ -8,7 +8,7 @@ export default function Person({person, onClick}) {
     const y = (person.position.y / MAX_HEIGHT) * 100;
     return (
         <div
-            className={classNames(styles.root, {[styles.sickPerson]: person.isSick})}
+            className={classNames(styles.root, {[styles.sickPerson]: person.isSick, [styles.boredPerson]: person.isBored})}
             style={{left: `${x}%`, top: `${y}%`}}
             onClick={() => onClick(person.id)}
         />
